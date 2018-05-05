8818e2b40c0f9fdd871d113e0c2fdb6dab75842c
Nazreen <nxmohamad@gmail.com>
2018-05-06 05:38:07 +0800

    turn off hashbang

--- src/router/index.js
+++ src/router/index.js
@@ -2,10 +2,12 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import Home from '@/components/Home'
+import Politicians from '@/components/Politicians'
 
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
@@ -16,6 +18,11 @@ export default new Router({
       path: '/help',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/politicians',
+      name: 'Politicians',
+      component: Politicians
     }
   ]
 })
