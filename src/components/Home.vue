07ee707378233cc50d06e889b5a96b64b2506cb7
Nazreen <nxmohamad@gmail.com>
2018-05-06 04:48:59 +0800

    move logo

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,22 +1,25 @@
 <template>
-  <div class="hello">
+  <main class="home">
     <h1>{{ msg }}</h1>
+    <img id="logo" src="@/assets/logo.jpg">
     <p>{{ description }}</p>
-  </div>
+    <el-button>View Promises</el-button>
+    <el-button>Submit a Promise</el-button>
+  </main>
 </template>
 
 <script>
 export default {
-  name: "HelloWorld",
-  data() {
+  name: 'HelloWorld',
+  data () {
     return {
       msg:
         "Tech has the potential to make our democracies better. It's time we realise that.",
       description:
-        "OpenPromises is civilian-led not for profit project that is based on recording promises made by politicians. Our source is publicly available media sources and we are currently focusing on Malaysia due to it being the homebase of our current participants, and also due to the upcoming general elections. We believe that technology can have a great impact on governance and democracy. Tech can change the world, and we aim to be a part of that."
-    };
+        'OpenPromises is civilian-led not for profit project that is based on recording promises made by politicians. Our source is publicly available media sources and we are currently focusing on Malaysia due to it being the homebase of our current participants, and also due to the upcoming general elections. We believe that technology can have a great impact on governance and democracy. Tech can change the world, and we aim to be a part of that.'
+    }
   }
-};
+}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
