0dec84c3ac6d20949eea2461f3b4da20ce7ea7f1
Nazreen <nxmohamad@gmail.com>
2018-05-06 05:58:24 +0800

    Create politician page (#5)
    * turn off hashbang
    
    * display politicians
    
    * move data
    
    * set link

---
+++ src/components/Politicians.vue
@@ -0,0 +1,74 @@
+<template>
+  <main id="politicians">
+    <h1>Politicians</h1>
+    <p>{{ description }}</p>
+    <el-row>
+  <el-col :span="6" v-for="o in politicians" :key="o">
+    <el-card :body-style="{ padding: '0px' }">
+        <a :href="/politician/ + o.id">
+      <img :src="o.profile_image" class="image">
+      <div style="padding: 14px;">
+        <span>{{ o.name  }}</span>
+        <div class="bottom clearfix">
+          <time class="time">{{ o.primary_position }}</time>
+        </div>
+      </div>
+      </a>
+    </el-card>
+  </el-col>
+</el-row>
+  </main>
+</template>
+
+<script>
+export default {
+  name: 'Politicians',
+  data () {
+    return {
+      politicians: require('../assets/politicians.json')
+  }
+}
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+a {
+    text-decoration: none
+}
+ .el-card {
+     height: 400px
+ }
+ .time {
+    font-size: 13px;
+    color: #999;
+  }
+  
+  .bottom {
+    margin-top: 13px;
+    line-height: 12px;
+  }
+
+  .button {
+    padding: 0;
+    float: right;
+  }
+
+  .image {
+    width: 100%;
+    display: block;
+    object-fit: cover; /* Do not scale the image */
+  object-position: top; /* Center the image within the element */
+  height: 300px;
+  }
+
+  .clearfix:before,
+  .clearfix:after {
+      display: table;
+      content: "";
+  }
+  
+  .clearfix:after {
+      clear: both
+  }
+</style>
