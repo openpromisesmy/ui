66a54998017816d35904c3b8257a29bb4f99f90e
Nazreen <nxmohamad@gmail.com>
2018-05-06 04:30:04 +0800

    update logo path

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,29 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <img id="logo" src="./assets/logo.jpg">
     <router-view/>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
-}
+  name: "App"
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+#logo {
+  max-width: 25%;
+  margin-bottom: 2em;
+  border-radius: 20px;
+  box-shadow: 3px 3px 7px 1px #999999;
 }
 </style>
