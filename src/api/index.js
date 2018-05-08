d9651af7ab89a346f297a2f241cffe4e0cda7825
Nazreen <nxmohamad@gmail.com>
2018-05-09 02:03:12 +0800

    feat(api): get promises (#12)
    * refactor get code
    
    * create getPromsies api
    
    * use getPromises api call in Promises

--- src/api/index.js
+++ src/api/index.js
@@ -1,16 +1,22 @@
 import axios from 'axios'
 const API_URL = 'https://us-central1-openpromises-8526c.cloudfunctions.net'
 
-async function getPoliticians () {
-  try {
-    const response = await axios.get(API_URL + '/politicians/')
-    const politicians = response.data
-    return politicians
-  } catch (e) {
-    console.error(e)
+function getSomething (path) {
+  return async function () {
+    try {
+      const response = await axios.get(API_URL + path)
+      const politicians = response.data
+      return politicians
+    } catch (e) {
+      console.error(e)
+    }
   }
 }
 
+const getPoliticians = getSomething('/politicians/')
+const getPromises = getSomething('/promises/')
+
 export {
-  getPoliticians
+  getPoliticians,
+  getPromises
 }
