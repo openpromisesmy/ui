0f9d921db53e5966ae7f6926abab6318524874cc
Nazreen <nxmohamad@gmail.com>
2018-05-09 02:05:44 +0800

    SubmitPromise uses actual politicians

--- src/components/SubmitPromise.vue
+++ src/components/SubmitPromise.vue
@@ -1,91 +1,95 @@
 <template>
   <main class="about">
     <h1 class="title">Submit A Promise</h1>
-            <el-form v-on:submit.prevent="onSubmit" :label-position="left" label-width="100px">
-          <el-row >
+    <p v-if="politicians.length == 0">Loading form...</p>
+    <el-form v-else v-on:submit.prevent="onSubmit" :label-position="left" label-width="100px">
+    <el-row >
 
-            <el-col :xs="24" :sm="12" >
-                <el-form-item label="Politician">
-              <el-select v-model="promise.politician_id">
-                <el-option
-                    default-first-option
-                    v-for="politician in politicians"
-                    :value="politician.id"
-                    :key="politician.id"
-                    :label="politician.name"
-                >
-                </el-option>
-              </el-select>
-                </el-form-item>
-            </el-col>
+      <el-col :xs="24" :sm="12" >
+          <el-form-item label="Politician">
+        <el-select v-model="promise.politician_id">
+          <el-option
+              default-first-option
+              v-for="politician in politicians"
+              :value="politician.id"
+              :key="politician.id"
+              :label="politician.name"
+          >
+          </el-option>
+        </el-select>
+          </el-form-item>
+      </el-col>
 
-            <el-col :xs="24" :sm="12" >
-                <el-form-item label="Category">
-              <el-input type="text" placeholder="enter text" v-model="promise.category"></el-input>
-                </el-form-item>
-            </el-col>
+      <el-col :xs="24" :sm="12" >
+          <el-form-item label="Category">
+        <el-input type="text" placeholder="enter text" v-model="promise.category"></el-input>
+          </el-form-item>
+      </el-col>
 
-            <el-col :xs="24" :sm="12" >
-                <el-form-item label="Title">
-              <el-input type="text" placeholder="enter text" v-model="promise.title"></el-input>
-                </el-form-item>
-            </el-col>
+      <el-col :xs="24" :sm="12" >
+          <el-form-item label="Title">
+        <el-input type="text" placeholder="enter text" v-model="promise.title"></el-input>
+          </el-form-item>
+      </el-col>
 
-            <el-col :xs="24" :sm="12" >
-                <el-form-item label="Status">
-              <el-input type="text" placeholder="enter text" v-model="promise.status"></el-input>
-                </el-form-item>
-            </el-col>
+      <el-col :xs="24" :sm="12" >
+          <el-form-item label="Status">
+        <el-input type="text" placeholder="enter text" v-model="promise.status"></el-input>
+          </el-form-item>
+      </el-col>
 
-            <el-col :xs="24" :sm="12" >
-                <el-form-item label="Cover Image">
-              <el-input type="text" placeholder="enter text" v-model="promise.cover_image"></el-input>
-                </el-form-item>
-            </el-col>
+      <el-col :xs="24" :sm="12" >
+          <el-form-item label="Cover Image">
+        <el-input type="text" placeholder="enter text" v-model="promise.cover_image"></el-input>
+          </el-form-item>
+      </el-col>
 
-            <el-col :xs="24" :sm="12" >
-                <el-form-item label="Source URL">
-              <el-input type="text" placeholder="enter text" v-model="promise.source_url"></el-input>
-                </el-form-item>
-            </el-col>
+      <el-col :xs="24" :sm="12" >
+          <el-form-item label="Source URL">
+        <el-input type="text" placeholder="enter text" v-model="promise.source_url"></el-input>
+          </el-form-item>
+      </el-col>
 
-            <el-col :xs="24" :sm="12" >
-                <el-form-item label="Source Name">
-              <el-input type="text" placeholder="enter text" v-model="promise.source_name"></el-input>
-                </el-form-item>
-            </el-col>
+      <el-col :xs="24" :sm="12" >
+          <el-form-item label="Source Name">
+        <el-input type="text" placeholder="enter text" v-model="promise.source_name"></el-input>
+          </el-form-item>
+      </el-col>
 
-            <el-col :xs="24" :sm="12" >
-                <el-form-item label="Source Date">
-              <el-input type="text" placeholder="enter text" v-model="promise.source_date"></el-input>
-                </el-form-item>
-            </el-col>
+      <el-col :xs="24" :sm="12" >
+          <el-form-item label="Source Date">
+        <el-input type="text" placeholder="enter text" v-model="promise.source_date"></el-input>
+          </el-form-item>
+      </el-col>
 
-            <el-col :span="24" >
-                <el-form-item label="Quote">
-              <el-input type="text" placeholder="enter text" v-model="promise.quote"></el-input>
-                </el-form-item>
-            </el-col>
+      <el-col :span="24" >
+          <el-form-item label="Quote">
+        <el-input type="text" placeholder="enter text" v-model="promise.quote"></el-input>
+          </el-form-item>
+      </el-col>
 
-            <el-col :xs="24" :sm="12" >
-                <el-form-item label="Notes">
-              <el-input type="text" placeholder="enter text" v-model="promise.notes"></el-input>
-                </el-form-item>
-            </el-col>
-          </el-row>
-                  <el-button v-on:click="onSubmit"> Submit </el-button>
-            </el-form>
-
+      <el-col :xs="24" :sm="12" >
+          <el-form-item label="Notes">
+        <el-input type="text" placeholder="enter text" v-model="promise.notes"></el-input>
+          </el-form-item>
+      </el-col>
+    </el-row>
+        <el-button v-on:click="onSubmit"> Submit </el-button>
+  </el-form>
   </main>
 </template>
 
 <script>
+import { getPoliticians } from '@/api'
 export default {
   name: 'SubmitPromise',
+  async created () {
+    this.politicians = await getPoliticians()
+  },
   data () {
     return {
       response: '',
-      politicians: require('../assets/politicians.json'), // TODO: replace with actual API call
+      politicians: [], // TODO: replace with actual API call
       promise: {
         politician_id: null, // select from database
         title: null, // string
