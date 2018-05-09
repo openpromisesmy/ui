f7423bf876e7300e46928f20a7149ea4fc9a246d
Nazreen <nxmohamad@gmail.com>
2018-05-09 16:22:11 +0800

    extract firebase init

---
+++ src/config/index.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase'
+var config = {
+  apiKey: 'AIzaSyBR0GKAzyozw4tp_6Q0kOso1pT9XyHWHpQ',
+  authDomain: 'openpromises-8526c.firebaseapp.com',
+  databaseURL: 'https://openpromises-8526c.firebaseio.com',
+  projectId: 'openpromises-8526c',
+  storageBucket: 'openpromises-8526c.appspot.com',
+  messagingSenderId: '100990292939'
+}
+firebase.initializeApp(config)
+
+export {
+    firebase
+}
