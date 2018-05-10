6bba4107c8b08909afb0962aa26b9dbc0c2b34e0
Nazreen <nxmohamad@gmail.com>
2018-05-11 04:09:06 +0800

    create Politician component

---
+++ src/components/Politician.vue
@@ -0,0 +1,28 @@
+<template>
+  <main id="politicians">
+    <h1>{{ politician.name }}</h1>
+  </main>
+</template>
+
+<script>
+import { getPolitician } from '@/api'
+export default {
+  name: 'Politician',
+  data () {
+    return {
+      politician: {}
+    }
+  },
+  async created () {
+    try {
+      this.politician = await getPolitician(this.$route.params.id)
+    } catch (e) {
+      console.error(e)
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
