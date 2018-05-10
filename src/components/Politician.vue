364ac78cc23626e974ba2a2c9f0062a5ac6e0428
Nazreen <nxmohamad@gmail.com>
2018-05-11 04:16:32 +0800

    display bio

--- src/components/Politician.vue
+++ src/components/Politician.vue
@@ -1,6 +1,9 @@
 <template>
   <main id="politicians">
     <h1>{{ politician.name }}</h1>
+    <img class="image" :src="politician.profile_image">
+    <p><b>{{ politician.primary_position }}</b></p>
+    <p>{{ politician.brief }}</p>
   </main>
 </template>
 
@@ -25,4 +28,15 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.image {
+  min-width: 30%;
+  display: inline-block;
+  object-fit: cover; /* Do not scale the image */
+  object-position: top; /* Center the image within the element */
+  height: 300px;
+}
+
+#politicians p b {
+  display: inline-block
+}
 </style>
