f13752746be012c554dc5c55be8dabc1b42e5c23
Nazreen <nxmohamad@gmail.com>
2018-05-21 02:50:42 +0800

    create Auth component

---
+++ src/components/Auth.vue
@@ -0,0 +1,20 @@
+<template>
+  <main class="auth">
+      <h1>Register/Login</h1>
+      <p>{{ authenticated ? email : 'Please login.' }}</p>
+  </main>
+</template>
+
+<script>
+export default {
+  name: 'Auth',
+  data () {
+    return {
+      authenticated: this.$store.state.user.authenticated
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
