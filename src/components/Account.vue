f0e079a1ddee979f983353b9c821fd38932f285f
Nazreen <nxmohamad@gmail.com>
2018-05-21 02:38:20 +0800

    create Account component

---
+++ src/components/Account.vue
@@ -0,0 +1,22 @@
+<template>
+  <main class="account">
+      <h1>Account</h1>
+      <p>{{ authenticated ? email : 'Please login.' }}</p>
+  </main>
+</template>
+
+<script>
+export default {
+  name: 'Account',
+  data () {
+    return {
+      authenticated: this.$store.state.user.authenticated,
+      email: this.$store.state.user.email
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
