8b0cb416976553b2e5373c7d0f94c3f0e531964c
Nazreen <nxmohamad@gmail.com>
2018-06-11 00:06:09 +0700

    feat(Home): include facebook Page plugin

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -5,6 +5,11 @@
     <p>{{ description }}</p>
     <router-link to="/promises"><el-button>View Promises</el-button></router-link>
     <router-link to="/submit"><el-button>Submit a Promise</el-button></router-link>
+    <div id="fb-root"></div>
+    <section>
+      <h1>Follow Us on Facebook (We post updates there first)</h1>
+      <div class="fb-page" data-href="https://www.facebook.com/openpromises.malaysia/" data-tabs="timeline" data-width="500" data-height="900" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/openpromises.malaysia/" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/openpromises.malaysia/">OpenPromises Malaysia</a></blockquote></div>
+    </section>
   </main>
 </template>
 
@@ -18,8 +23,17 @@ export default {
       description:
         'OpenPromises is civilian-led not for profit project that is based on recording promises made by politicians. Our source is publicly available media sources and we are currently focusing on Malaysia due to it being the homebase of our current participants, and also due to the upcoming general elections. We believe that technology can have a great impact on governance and democracy. Tech can change the world, and we aim to be a part of that.'
     }
-  }
-}
+  },
+  created () {
+    (function(d, s, id) {
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) return;
+      js = d.createElement(s); js.id = id;
+      js.src = 'https://connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v3.0&appId=1766611253591544&autoLogAppEvents=1';
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'))
+      }
+    }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
