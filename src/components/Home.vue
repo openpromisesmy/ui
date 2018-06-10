2e37c86f6e0eb0fa7e22f0fa09f00d12840af0a1
Nazreen <nxmohamad@gmail.com>
2018-06-11 03:58:22 +0800

    fix lint errors

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -25,15 +25,16 @@ export default {
     }
   },
   created () {
-    (function(d, s, id) {
-      var js, fjs = d.getElementsByTagName(s)[0];
-      if (d.getElementById(id)) return;
-      js = d.createElement(s); js.id = id;
-      js.src = 'https://connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v3.0&appId=1766611253591544&autoLogAppEvents=1';
-      fjs.parentNode.insertBefore(js, fjs);
+    (function (d, s, id) {
+      var js
+      var fjs = d.getElementsByTagName(s)[0]
+      if (d.getElementById(id)) return
+      js = d.createElement(s); js.id = id
+      js.src = 'https://connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v3.0&appId=1766611253591544&autoLogAppEvents=1'
+      fjs.parentNode.insertBefore(js, fjs)
     }(document, 'script', 'facebook-jssdk'))
-      }
-    }
+  }
+}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
