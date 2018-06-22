e5d0af0fa94b5a269a010b89cd17d66f6f65371a
Nazreen <nxmohamad@gmail.com>
2018-06-23 01:24:56 +0800

    feat(Auth): show loading spinner (#52)
    * show LoadingSpinner when popup is visible
    
    * proper checks for loading spinner

--- src/components/Auth.vue
+++ src/components/Auth.vue
@@ -1,26 +1,38 @@
 <template>
   <main class="auth">
-      <template v-if="!authenticated">
+      <template v-if="appStatus === 'popup'">
+        <LoadingSpinner />
+      </template>
+      <template v-else-if="!authenticated && appStatus === ''">
         <h1>Login or Sign Up</h1>
         <el-button type="primary" v-on:click="googleSignInHandler">Google Sign In</el-button>
+      </template>
+      <template v-else-if="appStatus === 'loggingIn'">
+        <p>Logging in...</p>
+        <LoadingSpinner />
       </template>
   </main>
 </template>
 
 <script>
 import { googleSignIn, getContributor, postContributor } from '@/api'
+import LoadingSpinner from '@/components/shared/LoadingSpinner'
 
 export default {
   name: 'Auth',
+  components: { LoadingSpinner },
   data () {
     return {
+      appStatus: '',
       authenticated: this.$store.state.user.authenticated
     }
   },
   methods: {
     googleSignInHandler: async function () {
       try {
+        this.appStatus = 'popup'
         const firebaseUser = await googleSignIn()
+        this.appStatus = 'loggingIn'
         const response = await getContributor(firebaseUser.email)
         // below: when first time signing in, create account
         let user = response[0]
