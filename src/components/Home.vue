c54bb59d5e435474dc31917dc3f05ab65dbb3559
Nazreen <nxmohamad@gmail.com>
2018-07-03 03:09:00 +0800

    reorganise cta

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,19 +1,30 @@
 <template>
   <main class="home">
-    <div><h1 id="tagline">{{ msg }}</h1></div>
-    <img id="logo" src="@/assets/logo.jpg">
-    <p>{{ description }}</p>
-    <router-link to="/promises"><el-button>View Promises</el-button></router-link>
-    <router-link to="/submit"><el-button>Submit a Promise</el-button></router-link>
-    <div id="fb-root"></div>
+
+    <section>
+      <div><h1 id="tagline">{{ tagline }}</h1></div>
+      <img id="logo" src="@/assets/logo.jpg">
+      <p>{{ description }}</p>
+      <div id="fb-root"></div>
+    </section>
+
     <section>
       <h1>View Promises by Politician</h1>
       <Politicians />
     </section>
+
+    <section id="cta">
+      <el-card>
+        <h1>Be part of the movement,</h1>
+        <router-link to="/submit"><el-button>Submit a Promise</el-button></router-link>
+      </el-card>
+    </section>
+
     <section>
       <h1>Follow Us on Facebook (We post updates there first)</h1>
       <div class="fb-page" data-href="https://www.facebook.com/openpromises.malaysia/" data-tabs="timeline" data-width="500" data-height="900" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/openpromises.malaysia/" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/openpromises.malaysia/">OpenPromises Malaysia</a></blockquote></div>
     </section>
+
   </main>
 </template>
 
@@ -25,8 +36,8 @@ export default {
   components: { Politicians },
   data () {
     return {
-      msg:
-        "Tech has the potential to make our democracies better. It's time we realise that.",
+      tagline:
+        "Track Promises by Your Representatives",
       description:
         'OpenPromises is civilian-led not for profit project that is based on recording promises made by politicians. Our source is publicly available media sources and we are currently focusing on Malaysia due to it being the homebase of our current participants, and also due to the upcoming general elections. We believe that technology can have a great impact on governance and democracy. Tech can change the world, and we aim to be a part of that.'
     }
@@ -54,5 +65,13 @@ export default {
   font-size: 18px;
   padding: 18px;
   border-radius: 18px
+}
+
+#cta {
+ margin: 50px 0
+}
+
+.el-card {
+  display: inline-block 
 }
 </style>
