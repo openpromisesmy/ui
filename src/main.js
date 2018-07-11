2ec2f97d27ec7088197c507b5be4e382b255bc8c
Nazreen Mohamad <nxmohamad@gmail.com>
2018-07-12 03:39:08 +0800

    setup vue-mq

--- src/main.js
+++ src/main.js
@@ -7,9 +7,17 @@ import ElementUI from 'element-ui'
 import locale from 'element-ui/lib/locale/lang/en'
 import 'element-ui/lib/theme-chalk/index.css'
 import store from './store'
+import VueMq from 'vue-mq'
 
 Vue.config.productionTip = false
 Vue.use(ElementUI, { locale })
+Vue.use(VueMq, {
+  breakpoints: {
+    sm: 450,
+    md: 1250,
+    lg: Infinity,
+  }
+})
 
 /* eslint-disable no-new */
 new Vue({
