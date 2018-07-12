aac2bf7ce53e134d16187f39c15b679827624fd5
Nazreen Mohamad <nxmohamad@gmail.com>
2018-07-12 18:19:49 +0800

    extract PoliticianDesktop

--- src/components/Politician.vue
+++ src/components/Politician.vue
@@ -1,67 +1,16 @@
 <template>
-  <main id="politicians">
-    <template v-if="Object.keys(politician) === 0">
-      <p>Loading politician...</p>
-    </template>
-    <template v-else>
-      <h1>{{ politician.name }}</h1>
-      <img class="image" :src="politician.profile_image">
-      <p><b>{{ politician.primary_position }}</b></p>
-      <h2>Promises by {{ politician.name }} {{ Array.isArray(promises) ? `(${promises.length})` : '' }}</h2>
-      <p>{{ politician.brief }}</p>
-    </template>
-
-    <template v-if="promises === 'loading'">
-      <p>Loading promises...This will take 2-4 seconds.</p>
-      <LoadingSpinner />
-    </template>
-    <template v-else>
-    <el-card id="Politician_stats">
-      <b>Promise Statistics:</b>
-      <el-button v-for="stat in stats" :key="stat.value">
-        <b>{{ stat.value }}</b> {{ stat.number }}
-      </el-button>
-    </el-card>
-    <el-table
-      :data="promises"
-      border
-      style="width: 100%">
-    <el-table-column
-      prop="title"
-      label="Title">
-      <template slot-scope="scope">
-        <router-link :to="'/promises/' + scope.row.id">{{ scope.row.title }}</router-link>
-      </template>
-    </el-table-column>
-    <el-table-column
-      prop="source_date"
-      label="Date"
-      width="150">
-    </el-table-column>
-    <el-table-column
-      prop="category"
-      label="Category"
-      width="180">
-    </el-table-column>
-    <el-table-column
-      prop="status"
-      label="Status"
-      width="125">
-    </el-table-column>
-  </el-table>
-    </template>
-  </main>
+  <politician-desktop v-bind="{ politician, promises, stats }" />
 </template>
 
 <script>
 import { getPolitician, getPoliticianPromises } from '@/api'
 import { generateStats } from '@/utils'
 import moment from 'moment'
-import LoadingSpinner from '@/components/shared/LoadingSpinner'
+import PoliticianDesktop from '@/components/PoliticianDesktop'
 
 export default {
   name: 'Politician',
-  components: { LoadingSpinner },
+  components: { PoliticianDesktop },
   data () {
     return {
       politician: {},
@@ -97,16 +46,4 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.image {
-  min-width: 30%;
-  display: inline-block;
-  object-fit: cover; /* Do not scale the image */
-  object-position: top; /* Center the image within the element */
-  height: 300px;
-}
-
-#politicians p b {
-  display: inline-block
-}
-
 </style>
