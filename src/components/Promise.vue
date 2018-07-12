86cc108341cc4b6c091da6cd6805b996c584f39f
Nazreen <nxmohamad@gmail.com>
2018-07-12 12:34:20 +0800

    now Promise fits in mobile wow (#66)
    * PromiseDesktop
    
    * PromiseMobile

--- src/components/Promise.vue
+++ src/components/Promise.vue
@@ -1,31 +1,17 @@
 <template>
-  <main id="Promise">
-    <template v-if="appStatus === 'loading'">
-      <p>Loading promise...</p>
-    </template>
-    <template v-else>
-      <h1>{{ promise.title }}</h1>
-      <el-row>
-        <el-col :span="12"><b>Made by</b></el-col>
-        <el-col :span="12">{{ politician.name }}</el-col>
-      </el-row>
-      <el-row v-for="(value, key) in displayedValues" :key="key" class="Promise_values">
-        <el-col :span="12"><div class="grid-content"> <b>{{ key }} </b> </div></el-col>
-        <el-col :span="12"><div class="grid-content"> {{ value }} </div></el-col>
-      </el-row>
-    </template>
-
-  </main>
+  <promise-mobile v-if="$mq === 'sm'" v-bind="{ promise, politician, appStatus, displayedValues }" />
+  <promise-desktop v-else v-bind="{ promise, politician, appStatus, displayedValues }" />
 </template>
 
 <script>
 import { getPromise, getPolitician } from '@/api'
+import PromiseDesktop from '@/components/PromiseDesktop'
+import PromiseMobile from '@/components/PromiseMobile'
 import { formatDate } from '@/utils'
-import LoadingSpinner from '@/components/shared/LoadingSpinner'
 
 export default {
   name: 'Promise',
-  components: { LoadingSpinner },
+  components: { PromiseDesktop, PromiseMobile },
   data () {
     return {
       appStatus: 'loading',
@@ -69,26 +55,4 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-#Promise {
-  max-width: 900px
-}
-
-.grid-content {
-  border-radius: 4px;
-  min-height: 36px;
-}
-
-.el-row {
-  padding: 10px;
-  border: 1px solid grey
-}
-
-.el-row:last-child {
-  margin-bottom: 0;
-}
-
-.el-col {
-  border-radius: 4px;
-}
-
 </style>
---
+++ src/components/PromiseDesktop.vue
@@ -0,0 +1,55 @@
+<template>
+  <main id="Promise">
+    <template v-if="appStatus === 'loading'">
+      <p>Loading promise...</p>
+    </template>
+    <template v-else>
+      <h1>{{ promise.title }}</h1>
+      <el-row>
+        <el-col :span="12"><b>Made by</b></el-col>
+        <el-col :span="12">{{ politician.name }}</el-col>
+      </el-row>
+      <el-row v-for="(value, key) in displayedValues" :key="key" class="Promise_values">
+        <el-col :span="12"><div class="grid-content"> <b>{{ key }} </b> </div></el-col>
+        <el-col :span="12"><div class="grid-content"> {{ value }} </div></el-col>
+      </el-row>
+    </template>
+
+  </main>
+</template>
+
+<script>
+import LoadingSpinner from '@/components/shared/LoadingSpinner'
+
+export default {
+  name: 'PromiseDesktop',
+  props: [ 'promise', 'politician', 'appStatus', 'displayedValues' ],
+  components: { LoadingSpinner }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+#Promise {
+  max-width: 900px
+}
+
+.grid-content {
+  border-radius: 4px;
+  min-height: 36px;
+}
+
+.el-row {
+  padding: 10px;
+  border: 1px solid grey
+}
+
+.el-row:last-child {
+  margin-bottom: 0;
+}
+
+.el-col {
+  border-radius: 4px;
+}
+
+</style>
