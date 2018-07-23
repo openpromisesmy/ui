6db16d88b66cf58d4666949092cb01cd8ac64a1e
Nazreen <nxmohamad@gmail.com>
2018-07-24 00:36:47 +0800

    move keys to env (#84)
    * move config to proper file
    
    * ignore .env, unignore package-lock
    
    * install dotenv
    
    * remove dotenv
    
    * no need to ignore .evn as we're not using it
    
    * update README
    
    * remove from repo
    
    * ignore config files
    
    * remove dotenv line
    
    * update README

--- README.md
+++ README.md
@@ -28,3 +28,21 @@ npm test
 ```
 
 For a detailed explanation on how things work, check out the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
+
+# Configuration
+
+Inside the `config` folder you need to create the following:
+- `prod.env.js`
+- `dev.env.js`
+- `test.env.js`
+The variables trickle down. From prod to dev, from dev to test.
+Set these variables:
+````
+NODE_ENV:
+GOOGLE_API_KEY: 
+FIREBASE_AUTH_DOMAIN: 
+FIREBASE_DATABASE_URL: 
+FIREBASE_PROJECT_ID: 
+FIREBASE_STORAGE_BUCKET: 
+FIREBASE_MESSAGING_SENDER_ID:
+````
--- src/config/index.js
+++ src/config/index.js
@@ -1,13 +1,14 @@
 import firebase from 'firebase'
 const config = {
-  apiKey: 'AIzaSyBR0GKAzyozw4tp_6Q0kOso1pT9XyHWHpQ',
-  authDomain: 'openpromises-8526c.firebaseapp.com',
-  databaseURL: 'https://openpromises-8526c.firebaseio.com',
-  projectId: 'openpromises-8526c',
-  storageBucket: 'openpromises-8526c.appspot.com',
-  messagingSenderId: '100990292939'
+  apiKey: process.env.GOOGLE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
 }
 firebase.initializeApp(config)
+console.log(process.env)
 
 let API_URL = 'https://us-central1-openpromises-8526c.cloudfunctions.net'
 // if (window.location.hostname === 'localhost') API_URL = 'http://localhost:5000/openpromises-8526c/us-central1'
