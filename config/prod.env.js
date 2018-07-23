02c3e97a444fc60033d00ca60ad8650f6e5d5751
Nazreen Mohamad <nxmohamad@gmail.com>
2018-07-24 00:17:10 +0800

    move config to proper file

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,4 +1,10 @@
 'use strict'
 module.exports = {
-  NODE_ENV: '"production"'
+  NODE_ENV: '"production"',
+  GOOGLE_API_KEY: '"AIzaSyBR0GKAzyozw4tp_6Q0kOso1pT9XyHWHpQ"',
+  FIREBASE_AUTH_DOMAIN: '"openpromises-8526c.firebaseapp.com"',
+  FIREBASE_DATABASE_URL: '"https://openpromises-8526c.firebaseio.com"',
+  FIREBASE_PROJECT_ID: '"openpromises-8526c"',
+  FIREBASE_STORAGE_BUCKET: '"openpromises-8526c.appspot.com"',
+  FIREBASE_MESSAGING_SENDER_ID: '"100990292939"'
 }
