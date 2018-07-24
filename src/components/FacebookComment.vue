cf110fc9d8032b1b70a4988054646b2971b2cf72
Nazreen Mohamad <nxmohamad@gmail.com>
2018-07-25 04:19:33 +0800

    lint

--- src/components/FacebookComment.vue
+++ src/components/FacebookComment.vue
@@ -12,13 +12,13 @@ export default {
   name: 'FacebookComment',
   mounted () {
     (function (d, s, id) {
-    var js
-    var fjs = d.getElementsByTagName(s)[0]
-    if (d.getElementById(id)) return
-    js = d.createElement(s); js.id = id
-    js.src = `https://connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v3.0&appId=${facebookAppId}&autoLogAppEvents=1`
-    fjs.parentNode.insertBefore(js, fjs)
-  }(document, 'script', 'facebook-jssdk'))
+      var js
+      var fjs = d.getElementsByTagName(s)[0]
+      if (d.getElementById(id)) return
+      js = d.createElement(s); js.id = id
+      js.src = `https://connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v3.0&appId=${facebookAppId}&autoLogAppEvents=1`
+      fjs.parentNode.insertBefore(js, fjs)
+    }(document, 'script', 'facebook-jssdk'))
   }
 }
 </script>
