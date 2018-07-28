c3ae5cecbe6bd47029c971d98a4c1c7e6d5e6895
Nazreen Mohamad <nxmohamad@gmail.com>
2018-07-29 04:07:00 +0800

    style

--- src/components/Politician/PromiseStats.vue
+++ src/components/Politician/PromiseStats.vue
@@ -1,9 +1,23 @@
 <template>
-    <el-card id="Politician_stats">
-      <b>Promise Statistics:</b>
-      <el-button v-for="(value, key) in stats" :key="key">
-        <b>{{ key }}</b> {{ value }}
-      </el-button>
+    <el-card id="Promise_stats">
+      <h2>{{ totalPromises }} promises made</h2>
+      <el-row v-for="(value, key) in stats" :key="key">
+          <el-col :span="14">
+            {{ key }}
+          </el-col>
+          <!-- <el-col :span="8">
+            <el-progress
+              :show-text="false"
+              :stroke-width="10"
+              color="darkslategrey"
+              :percentage="Math.round( (value/totalPromises)*100 )">
+            </el-progress>
+          </el-col> -->
+          <el-col :span="10">
+            <b>{{ value }}</b>
+          </el-col>
+
+      </el-row>
     </el-card>
 </template>
 
@@ -11,16 +25,23 @@
 export default {
   name: 'PromiseStats',
   props: ['stats'],
-  data () {
-    return {
+  computed: {
+    totalPromises () {
+      return Object.values(this.stats).reduce((a, b) => a + b)
     }
-  },
-  methods: {
-
   }
 }
 </script>
 
 <style scoped>
+#Promise_stats {
+  margin: 2px;
+  background-color: darkslategrey;
+  color: white
+}
+
+.el-row {
+  margin: 5px 0
+}
 
 </style>
