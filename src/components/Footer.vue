abbed6a0467bb55dd89a4d8657fd87dbfac85ace
Nazreen Mohamad <nxmohamad@gmail.com>
2018-08-20 02:04:30 +0800

    style

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,13 +1,32 @@
 <template>
-    <el-footer id="footer">
+    <footer id="footer">
         <el-row>
-            <el-col :span="8" v-for="(item, index) in footer" :key="index">
-                <div class="grid-content bg-purple">
-                <router-link :to="item.url">{{ item.text }}</router-link>
+            <el-col :xs="24" :lg="5" v-for="(item, index) in footer"
+                    :key="index">
+                <div>
+                <router-link :to="item.url"
+                              class="footer-link">
+                  {{ item.text }}
+                </router-link>
                 </div>
             </el-col>
+            <el-col :xs="24" :lg="5">
+                <a class="footer-link" href="https://facebook.com/openpromises.malaysia">
+                  Facebook Page
+                </a>
+            </el-col>
+            <el-col :xs="24" :lg="5">
+                <a class="footer-link" href="https://www.facebook.com/groups/1809117962724326/">
+                  Community Group
+                </a>
+            </el-col>
         </el-row>
-    </el-footer>
+        <el-row>
+          <el-col >
+            © 2018 OpenPromises
+          </el-col>
+        </el-row>
+    </footer>
 </template>
 
 <script>
@@ -30,8 +49,28 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
 #footer {
   margin-top: 20px;
+  background-color: darkslategray;
+  padding: 10px 5px;
+  color: white;
+}
+.footer-link {
+  color: white;
+  text-decoration: none;
+}
+.el-row {
+  display: flex;
+  align-items: center;
+  padding: 10px
+}
+
+@media only screen and (max-width: 600px) {
+    .el-row {
+      flex-direction: column;
+      text-align: left;
+      padding: 10px
+    }
 }
 </style>
