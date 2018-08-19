940122d8d181610cdd313459660e1e12fc0094ae
Nazreen Mohamad <nxmohamad@gmail.com>
2018-08-20 00:19:32 +0800

    create components for promise updates

---
+++ src/components/Promise/PromiseUpdateCard.vue
@@ -0,0 +1,42 @@
+<template>
+  <article>
+    <el-card>
+      <div slot="header">
+        <el-tag class="status">{{ promiseUpdate.status }}</el-tag>
+        {{ formatDate(promiseUpdate.source_date) }}
+        <span class="source">
+          Source:
+          <a :href="promiseUpdate.source_url">
+            {{ promiseUpdate.source_name }}
+          </a>
+        </span>
+      </div>
+      <p>
+      </p>
+      <h1> {{ promiseUpdate.title }}</h1>
+      <p>Description: {{ promiseUpdate.description }}</p>
+      <p>Quote: {{ promiseUpdate.quote }}</p>
+    </el-card>
+  </article>
+</template>
+
+<script>
+import { formatDate } from '@/utils'
+// TODO: color code for the status tags
+export default {
+  name: 'PromiseUpdateCard',
+  props: ['promiseUpdate'],
+  methods: {
+    formatDate
+  }
+}
+</script>
+
+<style scoped>
+.status {
+  margin-right: 20px
+}
+.source {
+  float: right
+}
+</style>
---
+++ src/components/Promise/PromiseUpdates.vue
@@ -0,0 +1,26 @@
+<template>
+  <main>
+      <promise-update-card
+        v-for="promiseUpdate in promiseUpdates"
+        :key="promiseUpdate.id"
+        v-if="promiseUpdates.length > 0"
+        :promiseUpdate="promiseUpdate"
+      />
+  </main>
+</template>
+
+<script>
+import PromiseUpdateCard from '@/components/Promise/PromiseUpdateCard'
+
+export default {
+  name: 'PromiseUpdates',
+  props: ['promiseUpdates', 'promiseID'],
+  components: { PromiseUpdateCard }
+}
+</script>
+
+<style scoped>
+main {
+  max-width: 900px
+}
+</style>
