7a444c7f0c69d15002dd055d64e61e9db040a4eb
Nazreen <nxmohamad@gmail.com>
2018-08-20 17:26:19 +0800

    feat(Promise/Desktop): include PoliticianDetails (#109)

---
+++ src/components/Politician/PoliticianDetails.vue
@@ -0,0 +1,47 @@
+<template>
+    <el-card id="PoliticianDetails">
+    <el-row>
+        <el-col :sm="10">
+            <img class="image" :src="politician.profile_image">
+        </el-col>
+         <el-col :sm="14">
+            <div class="details-text">
+                <h1>
+                    {{ politician.name }}
+                </h1>
+                <p><b>{{ politician.primary_position }}</b></p>
+                <p>{{ politician.brief }}</p>
+            </div>
+         </el-col>
+    </el-row>
+    </el-card>
+</template>
+
+<script>
+export default {
+  name: 'Politician',
+  props: ['politician']
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.el-card {
+    text-align: left;
+    height: 100%
+}
+
+.image {
+  display: block;
+  object-fit: cover; /* Do not scale the image */
+  object-position: top; /* Center the image within the element */
+  max-height: 250px;
+  max-width: 280px;
+  margin: 0 auto
+}
+
+.details-text {
+    margin: 0 auto
+}
+
+</style>
