e68e0237f78b0be3017ebb77d832c7d1de638bc5
Nazreen Mohamad <nxmohamad@gmail.com>
2018-09-06 01:54:59 +0800

    add community widget

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -27,6 +27,10 @@
         <h1>Help fund the movement</h1>
         <a href="https://www.patreon.com/openpromisesmalaysia"><el-button type="success">Check us out on Patreon</el-button></a>
       </el-card>
+      <el-card>
+        <h1>Check out our community</h1>
+        <a href="https://www.facebook.com/groups/op.malaysia.watchers/"><el-button type="primary">Visit Malaysia Watchers</el-button></a>
+      </el-card>
     </section>
 
     <section>
