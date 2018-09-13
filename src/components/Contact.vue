6f866846b665ae26c0f2aca104eced6bcccfdf67
Nazreen Mohamad <nxmohamad@gmail.com>
2018-09-14 04:29:00 +0800

    update link

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,7 +1,7 @@
 <template>
   <main>
     <h1 class="title">Contact OpenPromises</h1>
-    <p>To get in touch with us, <a href="http://facebook.com/OpenPromises" target="_blank">message us on Facebook</a>.</p>
+    <p>To get in touch with us, <a href="https://m.me/openpromises.malaysia" target="_blank">message us on Facebook</a>.</p>
   </main>
 </template>
 
