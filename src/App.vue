cc227ff21f3c3c27ee73626bc83e8a9c124d39e9
Nazreen Mohamad <nxmohamad@gmail.com>
2018-10-15 21:38:48 +0800

    remove junk

--- src/App.vue
+++ src/App.vue
@@ -46,18 +46,11 @@ export default {
 </script>
 
 <style>
-html, body {
-  height: 100%
-}
-
 #app {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
-  height: 100%;
-  display: flex;
-  flex-direction: column;
 }
 
 main {
