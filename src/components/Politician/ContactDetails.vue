b01f1829c20ea999228b2f8992ecbfb16487719a
Nazreen Mohamad <nxmohamad@gmail.com>
2019-01-16 07:26:40 +0800

    ok

--- src/components/Politician/ContactDetails.vue
+++ src/components/Politician/ContactDetails.vue
@@ -1,9 +1,13 @@
 <template>
   <el-card header="Contact Details">
-    <p><b>Email:</b> {{ email }} </p>
-    <p><b>Phone Number:</b> {{ phone_number }} </p>
-    <p><b>Facebook:</b> <a :href="facebook_url"> {{ facebook_url }} </a> </p>
-    <p><b>Twitter:</b> <a :href="twitter_url"> {{ twitter_url }} </a> </p>
+    <p><b>Email</b></p>
+    <p> {{ email || '-' }} </p>
+    <p><b>Phone Number</b></p>
+    <p> {{ phone_number || '-' }} </p>
+    <p><b>Facebook</b></p>
+    <p> <a :href="facebook_url"> {{ facebook_url || '-' }} </a> </p>
+    <p><b>Twitter</b></p>
+    <p><a :href="twitter_url"> {{ twitter_url || '-' }} </a> </p>
   </el-card>
 </template>
 
