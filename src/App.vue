84f8018b5e1f037e8573b50455870f89f99b055e
Nazreen Mohamad <nxmohamad@gmail.com>
2019-01-31 22:52:31 +0000

    remove

--- src/App.vue
+++ src/App.vue
@@ -3,15 +3,6 @@
     <Navbar />
     <el-main>
       <router-view/>
-      <!-- Load Facebook SDK for JavaScript -->
-      <div id="fb-root"></div>
-      <!-- Your customer chat code -->
-      <div class="fb-customerchat"
-        attribution=setup_tool
-        page_id="540152059524147"
-        logged_in_greeting="Hi. Itching to know something? Ask away!"
-        logged_out_greeting="Hi. Itching to know something? Ask away!">
-      </div>
     </el-main>
     <Footer class="footer-container"/>
   </el-container>
@@ -31,16 +22,6 @@ export default {
         { text: 'Politicians', url: '/politicians' }
       ]
     }
-  },
-  created () {
-    (function (d, s, id) {
-      var js
-      var fjs = d.getElementsByTagName(s)[0]
-      if (d.getElementById(id)) return
-      js = d.createElement(s); js.id = id
-      js.src = 'https://connect.facebook.net/en_GB/sdk/xfbml.customerchat.js#xfbml=1&version=v2.12&autoLogAppEvents=1'
-      fjs.parentNode.insertBefore(js, fjs)
-    }(document, 'script', 'facebook-jssdk'))
   }
 }
 </script>
