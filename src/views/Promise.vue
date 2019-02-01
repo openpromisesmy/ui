8601cdeaed06a952ffa1c961843d2aa2da289d6b
Nazreen Mohamad <nxmohamad@gmail.com>
2019-02-01 09:31:19 +0000

    fix css

--- src/views/Promise.vue
+++ src/views/Promise.vue
@@ -69,6 +69,12 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+#PromiseView {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 10px
+}
 #facebook-comment {
   text-align: center
 }
