091248e8c02aba5042ccd15e8971fc185f6fbf4b
Too <sengweitoo@gmail.com>
2019-02-04 18:25:25 +0800

    fix(Navbar): does not collapse after navigating in mobile #164

--- src/components/Navbar/Mobile.vue
+++ src/components/Navbar/Mobile.vue
@@ -1,41 +1,41 @@
 <template>
   <el-collapse v-model="activeNames" id="navbar">
-  <img id="navbar-logo" src="@/assets/openpromises.png"/>
+    <img id="navbar-logo" src="@/assets/openpromises.png"/>
 
-  <el-collapse-item title="Menu" name="1">
-    <el-row class="tac">
-  <el-col>
-    <el-menu
-      default-active="2"
-      class="el-menu-vertical-demo"
-      @select="handleClose"
-      @open="handleOpen"
-      @close="handleClose"
-      :collapse="isCollapse"
-      >
-        <router-link v-bind:to="item.url" v-for="(item, index) in navigation" :key="index">
-            <el-menu-item  v-bind:index="String(index)">
-              {{ item.text }}
+    <el-collapse-item title="Menu" name="1">
+      <el-row class="tac">
+        <el-col>
+          <el-menu
+            default-active="2"
+            class="el-menu-vertical-demo"
+            @select="handleSelect"
+            @open="handleOpen"
+            @close="handleClose"
+            :collapse="isCollapse"
+            >
+            <router-link v-bind:to="item.url" v-for="(item, index) in navigation" :key="index">
+                <el-menu-item  v-bind:index="String(index)">
+                  {{ item.text }}
+                </el-menu-item>
+            </router-link>
+            <el-menu-item index="4" id="account" >
+              <router-link to="/account">
+                {{ this.$store.state.user.authenticated ? email : 'Login' }}
+              </router-link>
             </el-menu-item>
-        </router-link>
-        <el-menu-item index="4" id="account" >
-          <router-link to="/account">
-            {{ this.$store.state.user.authenticated ? email : 'Login' }}
-          </router-link>
-        </el-menu-item>
-        <el-menu-item index="5" id="submit" >
-          <router-link to="/submit">
-            <el-button type="primary">
-              Submit A Promise
-            </el-button>
-          </router-link>
-        </el-menu-item>
-    </el-menu>
-  </el-col>
-</el-row>
-  </el-collapse-item>
+            <el-menu-item index="5" id="submit" >
+              <router-link to="/submit">
+                <el-button type="primary">
+                  Submit A Promise
+                </el-button>
+              </router-link>
+            </el-menu-item>
+          </el-menu>
+        </el-col>
+      </el-row>
+    </el-collapse-item>
 
-</el-collapse>
+  </el-collapse>
 </template>
 
 <script>
@@ -44,19 +44,19 @@ export default {
   props: ['navigation', 'authenticated', 'email'],
   data () {
     return {
-      isCollapse: false,
       activeNames: []
     }
   },
   methods: {
     handleSelect (key, keyPath) {
-      // this.isCollapse = true
+      // Collapse Nav Bar
+      this.activeNames = []
     },
     handleOpen (key, keyPath) {
-      // this.isCollapse = false
+      // Do something
     },
     handleClose (key, keyPath) {
-      // this.isCollapse = true
+      // Do something
     }
   }
 }
