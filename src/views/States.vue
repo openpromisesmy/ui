58884f34302373f64dcc960f84417cb3149c8452
weiweitoo <sengweitoo@gmail.com>
2019-03-13 03:35:15 +0800

    fix(states): roughly polish state pages

--- src/views/States.vue
+++ src/views/States.vue
@@ -1,8 +1,19 @@
 <template>
   <main id="states">
 
-  <el-col v-for="state in malaysianStates" :key="state" :sm="6" :xs="24" class="state">
-    <router-link :to="`/states/${state}`">{{ state }}</router-link>
+  <el-col v-for="state in malaysianStates" :key="state" :lg="4" :sm="6" :xs="24" class="state">
+    <el-row>
+      <el-card shadow="hover" :body-style="{'padding' : '0', 'position' : 'relative'}" :style="{'margin' : '5px' }">
+        <div class="card-body">
+          <router-link :to="`/states/${state.name}`">
+            <img class="card-image" :src=state.image>
+            <div class="card-info">
+              <p>{{ state.name  }}</p>
+            </div>
+          </router-link>
+        </div>
+      </el-card>
+    </el-row>
   </el-col>
 
   </main>
@@ -27,8 +38,18 @@ main {
   text-align: center
 }
 
-.state {
-  padding: 20px
+div.el-card__body{
+  padding: 0;
+  margin-top: 20px;
+}
+
+.card-image{
+  max-width: 100%;
+}
+
+.card-body a{
+  text-decoration: none;
+  color: black;
 }
 
 </style>
