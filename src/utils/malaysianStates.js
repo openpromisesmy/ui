d6385ecfb9d9cd1a7c42241af7af40e5ca2576c3
weiweitoo <sengweitoo@gmail.com>
2019-03-13 03:35:15 +0800

    Roughly Polish state pages

--- src/utils/malaysianStates.js
+++ src/utils/malaysianStates.js
@@ -1,20 +1,68 @@
 const malaysianStates = [
-  'Johor',
-  'Kedah',
-  'Kelantan',
-  'Kuala Lumpur',
-  'Labuan',
-  'Melaka',
-  'Negeri Sembilan',
-  'Pahang',
-  'Perak',
-  'Perlis',
-  'Pulau Pinang',
-  'Putrajaya',
-  'Sabah',
-  'Sarawak',
-  'Selangor',
-  'Terengganu'
+  {
+    'name': 'Johor',
+    'image': 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/5a/Flag_of_Johor.svg/1200px-Flag_of_Johor.svg.png'
+  },
+  {
+    'name': 'Kedah',
+    'image': 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cc/Flag_of_Kedah.svg/1200px-Flag_of_Kedah.svg.png'
+  },
+  {
+    'name': 'Kelantan',
+    'image': 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/Flag_of_Kelantan.svg/1200px-Flag_of_Kelantan.svg.png'
+  },
+  {
+    'name': 'Kuala Lumpur',
+    'image': 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/64/Flag_of_Kuala_Lumpur%2C_Malaysia.svg/1200px-Flag_of_Kuala_Lumpur%2C_Malaysia.svg.png'
+  },
+  {
+    'name': 'Labuan',
+    'image': 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/Flag_of_Labuan.svg/1200px-Flag_of_Labuan.svg.png'
+  },
+  {
+    'name': 'Melaka',
+    'image': 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/09/Flag_of_Malacca.svg/1200px-Flag_of_Malacca.svg.png'
+  },
+  {
+    'name': 'Negeri Sembilan',
+    'image': 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/db/Flag_of_Negeri_Sembilan.svg/1200px-Flag_of_Negeri_Sembilan.svg.png'
+  },
+  {
+    'name': 'Pahang',
+    'image': 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/aa/Flag_of_Pahang.svg/1200px-Flag_of_Pahang.svg.png'
+  },
+  {
+    'name': 'Perak',
+    'image': 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/87/Flag_of_Perak.svg/1200px-Flag_of_Perak.svg.png'
+  },
+  {
+    'name': 'Pulau Pinang',
+    'image': 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d4/Flag_of_Penang_%28Malaysia%29.svg/1200px-Flag_of_Penang_%28Malaysia%29.svg.png'
+  },
+  {
+    'name': 'Perlis',
+    'image': 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/aa/Flag_of_Perlis.svg/1200px-Flag_of_Perlis.svg.png'
+  },
+  {
+    'name': 'Putrajaya',
+    'image': 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/9f/Flag_of_Putrajaya.svg/1200px-Flag_of_Putrajaya.svg.png'
+  },
+  {
+    'name': 'Sabah',
+    'image': 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b5/Flag_of_Sabah.svg/1200px-Flag_of_Sabah.svg.png'
+  },
+  {
+    'name': 'Sarawak',
+    'image': 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/7e/Flag_of_Sarawak.svg/1200px-Flag_of_Sarawak.svg.png'
+  },
+  {
+    'name': 'Selangor',
+    'image': 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/0c/Flag_of_Selangor.svg/1200px-Flag_of_Selangor.svg.png'
+  },
+  {
+    'name': 'Terengganu',
+    'image': 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/Flag_of_Terengganu.svg/1200px-Flag_of_Terengganu.svg.png'
+  }
 ]
 
 export default malaysianStates
