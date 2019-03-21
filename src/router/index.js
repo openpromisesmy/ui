4e49d992a747bd0568bea26b3bb134ff316fe0dc
Nazreen <nxmohamad@gmail.com>
2019-03-22 01:40:11 +0800

    feat: 404 page (#171)
    * route
    
    * check
    
    * component
    
    * s

--- src/router/index.js
+++ src/router/index.js
@@ -14,6 +14,7 @@ import About from '@/views/About'
 import SubmitPromise from '@/views/SubmitPromise'
 import Account from '@/views/Account'
 import Contact from '@/views/Contact'
+import Oops from '@/components/404'
 import beforeEach from './beforeEach'
 
 Vue.use(Router)
@@ -22,7 +23,10 @@ const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home
+    component: Home,
+    meta: {
+      title: 'Keep power in check'
+    }
   },
   {
     path: '/politicians',
@@ -111,6 +115,14 @@ const routes = [
     component: Account,
     meta: {
       title: 'Account'
+    }
+  },
+  {
+    path: '/404',
+    name: '404',
+    component: Oops,
+    meta: {
+      title: '404'
     }
   }
 ]
