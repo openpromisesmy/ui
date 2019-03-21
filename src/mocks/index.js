ae9b043524cba05bf5d3ecf8b7c436cbb9f24d60
Nazreen Mohamad <nxmohamad@gmail.com>
2019-03-22 00:54:32 +0800

    copy in mock lists

---
+++ src/mocks/index.js
@@ -0,0 +1,31 @@
+const lists = [
+  {
+    id: 'm54ertdfgcv',
+    live: false,
+    title: 'Student Aids',
+    contributor_id: '-LAEDTAC6HQeWYrV3atG',
+    created_at: "2019-03-17T18:35:05.441Z",
+    updated_at: "2019-03-17T18:35:05.441Z",
+    promise_ids: [
+      'CeX1MLvSq7IVx3nI1CgI',
+      'U5zwNgRbdu0NZRjFLoBF',
+      'o3lyMjqZrCALP6HSUZFS'
+    ]
+  },
+  {
+    id: 'ytughjbsdf8t7yiu',
+    live: true,
+    title: 'Broadband service in Malaysia',
+    contributor_id: '-LAEDTAC6HQeWYrV3atG',
+    created_at: "2019-03-17T18:35:05.441Z",
+    updated_at: "2019-03-17T18:35:05.441Z",
+    promise_ids: [
+      'XjGLsRtJ9spk5VeBidDl',
+      'bGvbmCPqIcpWQIZAp92C'
+    ]
+  }
+]
+
+export default {
+  lists
+}
