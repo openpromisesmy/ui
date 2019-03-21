cf8c86eef5fe7fb0b7b3ad1302f3639d9a8c5bea
Nazreen Mohamad <nxmohamad@gmail.com>
2019-03-22 01:14:04 +0800

    init view

---
+++ src/views/Lists.vue
@@ -0,0 +1,48 @@
+<template>
+  <main>
+    <header>
+      <h1>Lists</h1>
+      <p>Promises grouped by themes.</p>
+    </header>
+    <lists-table :lists="lists"/>
+  </main>
+</template>
+
+<script>
+import { listLists } from '@/api'
+import ListsTable from '@/components/Lists/Table'
+
+export default {
+  name: 'Lists',
+  components: { ListsTable },
+  data () {
+    return {
+      appStatus: '',
+      lists: []
+    }
+  },
+  methods: {
+    async listListsHandler () {
+      return listLists()
+    }
+  },
+  async created () {
+    try {
+      this.appStatus = 'loading'
+      this.lists = await this.listListsHandler()
+      this.appStatus = ''
+    } catch (e) {
+      console.error(e)
+    }
+  }
+}
+</script>
+
+<style scoped>
+main {
+  padding: 20px;
+}
+header {
+  text-align: center;
+}
+</style>
