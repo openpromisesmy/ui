334675cde4f338fd93ae99a0388b6f1122a5f03f
Nazreen Mohamad <nxmohamad@gmail.com>
2019-04-12 01:32:07 +0800

    (List): display description

--- src/views/List.vue
+++ src/views/List.vue
@@ -5,50 +5,51 @@
     </template>
     <template v-else>
       <h1>{{ list.title }}</h1>
+      <p>{{ list.description }}</p>
       <promises-table :promises="promises" :exclude="['source_name', 'politician_name']"/>
     </template>
   </div>
 </template>
 
 <script>
-import { getList, getPromise } from '@/api'
-import PromisesTable from '@/components/PromisesTable'
-import LoadingSpinner from '@/components//LoadingSpinner'
+import { getList, getPromise } from "@/api";
+import PromisesTable from "@/components/PromisesTable";
+import LoadingSpinner from "@/components//LoadingSpinner";
 
 export default {
-  name: 'List',
-  data () {
+  name: "List",
+  data() {
     return {
-      appStatus: 'loading',
+      appStatus: "loading",
       list: {},
       promises: []
-    }
+    };
   },
   components: { LoadingSpinner, PromisesTable },
-  async created () {
+  async created() {
     try {
-      this.list = await this.getListHandler(this.$route.params.id)
+      this.list = await this.getListHandler(this.$route.params.id);
       this.list.promise_ids.forEach(async promiseId => {
-        this.promises.push(await getPromise(promiseId))
-      })
-      this.appStatus = ''
+        this.promises.push(await getPromise(promiseId));
+      });
+      this.appStatus = "";
     } catch (e) {
-      console.error(e)
+      console.error(e);
     }
   },
   methods: {
-    async getListHandler (id) {
-      return getList(id)
+    async getListHandler(id) {
+      return getList(id);
     },
-    async getPromiseHandler (id) {
-      return getPromise(id)
+    async getPromiseHandler(id) {
+      return getPromise(id);
     }
   }
-}
+};
 </script>
 
 <style scoped>
 h1 {
-  text-align: center
+  text-align: center;
 }
 </style>
