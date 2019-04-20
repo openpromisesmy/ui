fbb1aaf25f37a1e2a296a1d922737782d07f390e
Nazreen Mohamad <nxmohamad@gmail.com>
2019-04-20 13:43:13 +0800

    extract to component

---
+++ src/components/Quizzes/RealOrFake.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <div
+      class="typeform-widget"
+      data-url="https://openpromisesmy.typeform.com/to/rthWN7"
+      style="max-width: 700px; height: 500px; margin: 10px auto"
+    ></div>
+    <div
+      style="font-family: Sans-Serif;font-size: 12px;color: #999;opacity: 0.5; padding-top: 5px;"
+    >
+      powered by
+      <a
+        href="https://admin.typeform.com/signup?utm_campaign=rthWN7&utm_source=typeform.com-13783432-Basic&utm_medium=typeform&utm_content=typeform-embedded-poweredbytypeform&utm_term=EN"
+        style="color: #999"
+        target="_blank"
+      >Typeform</a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'RealOrFake',
+  created () {
+    (function () { var qs, js, q, s, d = document, gi = d.getElementById, ce = d.createElement, gt = d.getElementsByTagName, id = 'typef_orm', b = 'https://embed.typeform.com/'; if (!gi.call(d, id)) { js = ce.call(d, 'script'); js.id = id; js.src = b + 'embed.js'; q = gt.call(d, 'script')[0]; q.parentNode.insertBefore(js, q) } })()
+  }
+}
+</script>
