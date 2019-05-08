b1e14100ea05db6fc34a8f65735bd27221a760da
Nazreen <nxmohamad@gmail.com>
2019-05-09 00:00:03 +0800

    feat(navbar): change email into standard text (#191)
    * remove email
    
    * email to account
    
    * remove dulplicate + all promises page

--- src/components/Navbar/Mobile.vue
+++ src/components/Navbar/Mobile.vue
@@ -1,6 +1,6 @@
 <template>
   <el-collapse v-model="activeNames" id="navbar">
-    <img id="navbar-logo" src="@/assets/openpromises.png"/>
+    <img id="navbar-logo" src="@/assets/openpromises.png">
 
     <el-collapse-item title="Menu" name="1">
       <el-row class="tac">
@@ -12,73 +12,70 @@
             @open="handleOpen"
             @close="handleClose"
             :collapse="isCollapse"
+          >
+            <el-menu-item
+              v-bind:index="String(index)"
+              v-for="(item, index) in navigation"
+              :key="index"
             >
-              <el-menu-item  v-bind:index="String(index)" v-for="(item, index) in navigation" :key="index">
-                <router-link v-if="item.url" v-bind:to="item.url">
-                  {{ item.text }}
-                </router-link>
-                <a v-else-if="item.externalUrl" :href="item.externalUrl">
-                  {{ item.text }}
-                </a>
-              </el-menu-item>
-            <el-menu-item index="4" id="account" >
-              <router-link to="/account">
-                {{ this.$store.state.user.authenticated ? email : 'Login' }}
-              </router-link>
+              <router-link v-if="item.url" v-bind:to="item.url">{{ item.text }}</router-link>
+              <a v-else-if="item.externalUrl" :href="item.externalUrl">{{ item.text }}</a>
             </el-menu-item>
-            <el-menu-item index="5" id="submit" >
+            <el-menu-item index="4" id="account">
+              <router-link
+                to="/account"
+              >{{ this.$store.state.user.authenticated ? 'Account' : 'Login' }}</router-link>
+            </el-menu-item>
+            <el-menu-item index="5" id="submit">
               <router-link to="/submit">
-                <el-button type="primary">
-                  Submit A Promise
-                </el-button>
+                <el-button type="primary">Submit A Promise</el-button>
               </router-link>
             </el-menu-item>
           </el-menu>
         </el-col>
       </el-row>
     </el-collapse-item>
-
   </el-collapse>
 </template>
 
 <script>
 export default {
-  name: 'NavbarMobile',
-  props: ['navigation', 'authenticated', 'email'],
-  data () {
+  name: "NavbarMobile",
+  props: ["navigation", "authenticated", "email"],
+  data() {
     return {
       activeNames: []
-    }
+    };
   },
   methods: {
-    handleSelect (key, keyPath) {
+    handleSelect(key, keyPath) {
       // Collapse Nav Bar
-      this.activeNames = []
+      this.activeNames = [];
     },
-    handleOpen (key, keyPath) {
+    handleOpen(key, keyPath) {
       // Do something
     },
-    handleClose (key, keyPath) {
+    handleClose(key, keyPath) {
       // Do something
     }
   }
-}
+};
 </script>
 
 <style scoped>
 #navbar {
-  text-align: center
+  text-align: center;
 }
 
 a {
   text-decoration: none;
-  font-size: 1rem
+  font-size: 1rem;
 }
 #navbar-logo {
   height: 32px;
   float: left;
   margin-top: 8px;
-  margin-left: 12px
+  margin-left: 12px;
 }
 .router-link-active {
   font-weight: 800;
