dadfc51da4e4796237b4adcdbab87c2304c9d3e8
Nazreen <nxmohamad@gmail.com>
2019-05-08 23:58:40 +0800

    email to account

--- src/components/Navbar/Desktop.vue
+++ src/components/Navbar/Desktop.vue
@@ -1,60 +1,57 @@
 <template>
   <el-header id="navbar">
-        <img id="navbar-logo"  src="@/assets/openpromises.png"/>
+    <img id="navbar-logo" src="@/assets/openpromises.png">
 
-      <el-menu :default-active="activeIndex" mode="horizontal" @select="handleSelect">
-        <el-menu-item v-for="(item, index) in navigation" :key="index" v-bind:index="String(index)">
-          <router-link v-if="item.url" v-bind:to="item.url">{{ item.text }}</router-link>
-          <a v-else-if="item.externalUrl" :href="item.externalUrl">{{ item.text }}</a>
-        </el-menu-item>
-        <el-menu-item index="5" id="account">
-          <router-link to="/account">
-            {{ this.$store.state.user.authenticated ? email : 'Login' }}
-          </router-link>
-        </el-menu-item>
-        <el-menu-item index="6" id="submit">
-          <router-link to="/submit">
-            <el-button type="primary">
-              Submit A Promise
-            </el-button>
-          </router-link>
-        </el-menu-item>
+    <el-menu :default-active="activeIndex" mode="horizontal" @select="handleSelect">
+      <el-menu-item v-for="(item, index) in navigation" :key="index" v-bind:index="String(index)">
+        <router-link v-if="item.url" v-bind:to="item.url">{{ item.text }}</router-link>
+        <a v-else-if="item.externalUrl" :href="item.externalUrl">{{ item.text }}</a>
+      </el-menu-item>
+      <el-menu-item index="5" id="account">
+        <router-link to="/account">{{ this.$store.state.user.authenticated ? 'Account' : 'Login' }}</router-link>
+      </el-menu-item>
+      <el-menu-item index="6" id="submit">
+        <router-link to="/submit">
+          <el-button type="primary">Submit A Promise</el-button>
+        </router-link>
+      </el-menu-item>
     </el-menu>
   </el-header>
 </template>
 
 <script>
 export default {
-  name: 'NavbarDesktop',
-  props: ['navigation', 'authenticated', 'email'],
-  data () {
+  name: "NavbarDesktop",
+  props: ["navigation", "authenticated", "email"],
+  data() {
     return {
-      activeIndex: '0'
-    }
+      activeIndex: "0"
+    };
   },
   methods: {
-    handleSelect (key, keyPath) {
-      console.log(key, keyPath)
+    handleSelect(key, keyPath) {
+      console.log(key, keyPath);
     }
   }
-}
+};
 </script>
 
 <style scoped>
 a {
   text-decoration: none;
-  font-size: 1rem
+  font-size: 1rem;
 }
 #navbar-logo {
   margin: 7px 12px 7px 12px;
   float: left;
-  height: 45px
+  height: 45px;
 }
-#submit, #account {
-  float:right
+#submit,
+#account {
+  float: right;
 }
-.el-menu{
-    background-color: transparent;
+.el-menu {
+  background-color: transparent;
 }
 .router-link-active {
   font-weight: 600;
