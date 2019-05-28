f3efa4c8324daa2115849432ffa60c1f65f1ffe0
Nazreen <nxmohamad@gmail.com>
2019-05-29 01:16:22 +0800

    remove unused

--- src/components/Navbar/index.vue
+++ src/components/Navbar/index.vue
@@ -15,7 +15,6 @@ export default {
   },
   data () {
     return {
-      activeIndex: '0',
       navigation: [
         { text: 'Home', url: '/' },
         { text: 'Politicians', url: '/politicians' },
