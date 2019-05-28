2017a60f0363d5050b2bb7d9c909af2a541b9ad6
Nazreen <nxmohamad@gmail.com>
2019-05-29 01:40:54 +0800

    refactor(Home): take out hero and refactor (#200)
    * quick fix
    
    * take logo styling out
    
    * take Hero out

--- src/components/Navbar/index.vue
+++ src/components/Navbar/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <navbar-mobile v-if="$mq === 'navbar'" v-bind="{ navigation, authenticated }"/>
+  <navbar-mobile v-if="$mq === 'navbar' || $mq === 'sm'" v-bind="{ navigation, authenticated }"/>
   <navbar-desktop v-else v-bind="{ navigation, authenticated }"/>
 </template>
 
