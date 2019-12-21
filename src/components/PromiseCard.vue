675d958b13acefa03eb5c35a186ac8482184ac52
Nazreen <nxmohamad@gmail.com>
2019-12-21 20:57:46 +0000

    feat: cherry pick Promise Card  (#215)
    * BAM!
    
    * feat(Promise): use status colour in hero (#193)
    
    * add color to promise hero
    
    * update layout
    
    * fix(PromiseCard): TBC hack
    
    * upgrade to latest vue-loader (#214)
    
    * refresh package-lock

---
+++ src/components/PromiseCard.vue
@@ -0,0 +1,55 @@
+<template>
+  <router-link :to="'/promises/' + promise.id">
+  <el-card class="promise-card" :style="'background-color:' + statusColorMap[promise.status]">
+    <header>
+      <span class="status">{{ promise.status === 'TBC' ? 'Review Needed' : promise.status }}</span>
+      <span class="source-date">{{ formatDate(promise.source_date) }}</span>
+    </header>
+    <h1>{{ promise.title }}</h1>
+    <p class="category">{{ promise.category }}</p>
+  </el-card>
+  </router-link>
+</template>
+
+<script>
+import { formatDate, statusColorMap } from '@/utils'
+
+export default {
+  name: 'PromiseCard',
+  props: ['promise'],
+  data: () => ({
+    statusColorMap
+  }),
+  methods: {
+    formatDate
+  }
+}
+</script>
+
+<style scoped>
+a {
+  text-decoration: none
+}
+.promise-card {
+  width: 250px;
+  height: 300px;
+  color: white;
+  position: relative;
+  margin: 10px auto;
+}
+header {
+  font-size: 0.8rem;
+}
+.status {
+  font-weight: 900;
+  text-transform: uppercase
+}
+.source-date {
+  float: right
+}
+.category {
+  position: absolute;
+  bottom: 0;
+}
+
+</style>
---
+++ src/utils/statusColorMap.js
@@ -0,0 +1,11 @@
+const statusColorMap = {
+  'TBC': 'grey',
+  'Review Needed': 'grey',
+  'Not Started': '#5271ff',
+  'In Progress': '#ffac33',
+  'Fulfilled': '#35b729',
+  'At Risk': '#ed2022',
+  'Broken': 'black'
+}
+
+export default statusColorMap
