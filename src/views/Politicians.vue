c6684faaa376d2b4d8a18b4b374bab9105522bcb
Nazreen Mohamad <nxmohamad@gmail.com>
2020-03-12 01:52:05 +0000

    reduce size

--- src/views/Politicians.vue
+++ src/views/Politicians.vue
@@ -7,12 +7,15 @@
     <el-input clearable placeholder="Search for a politician" v-model="search" class="search" />
 
     <el-row class="politicians-row">
-      <el-col :xs="12" :sm="8" :md="6" :lg="6" :xl="4" v-for="o in filteredPoliticians" :key="o.id" >
-        <el-card shadow="hover" :style="{'background' : 'url(' + o.profile_image + ') no-repeat center center', 'background-size' : 'cover', 'position' : 'relative','margin' : '3px' }" >
+      <el-col :xs="12" :sm="6" :md="4" v-for="o in filteredPoliticians" :key="o.id">
+        <el-card
+          shadow="hover"
+          :style="{'background' : 'url(' + o.profile_image + ') no-repeat center center', 'background-size' : 'cover', 'position' : 'relative','margin' : '3px' }"
+        >
           <div class="card-body">
             <router-link :to="/politician/ + o.id">
               <div class="card-info">
-                <span>{{ o.name  }}</span>
+                <span>{{ o.name }}</span>
                 <div class="bottom clearfix">
                   <time class="time">{{ o.primary_position }}</time>
                 </div>
@@ -24,52 +27,57 @@
     </el-row>
 
     <p v-if="filteredPoliticians.length === 0">Sorry, no politicians match your search.</p>
-
   </main>
 </template>
 
 <script>
-import { getPoliticians } from '@/api'
-import { loadCache, updateCache } from '@/utils'
-import LoadingSpinner from '@/components//LoadingSpinner'
+import { getPoliticians } from "@/api";
+import { loadCache, updateCache } from "@/utils";
+import LoadingSpinner from "@/components//LoadingSpinner";
 
 export default {
-  name: 'Politicians',
-  data () {
+  name: "Politicians",
+  data() {
     return {
       politicians: [],
-      search: ''
-    }
+      search: ""
+    };
   },
   components: { LoadingSpinner },
   computed: {
-    filteredPoliticians () {
+    filteredPoliticians() {
       return this.politicians.filter(politician => {
-        return politician.name.toLowerCase().includes(this.search.toLowerCase())
-      })
+        return politician.name
+          .toLowerCase()
+          .includes(this.search.toLowerCase());
+      });
     }
   },
-  async created () {
+  async created() {
     try {
-      this.politicians = await loadCache(this, 'politicians', getPoliticians())
+      this.politicians = await loadCache(this, "politicians", getPoliticians());
     } catch (e) {
-      console.log(e)
+      console.log(e);
     }
   },
-  async mounted () {
+  async mounted() {
     try {
-      this.politicians = await updateCache(this, 'politicians', getPoliticians())
+      this.politicians = await updateCache(
+        this,
+        "politicians",
+        getPoliticians()
+      );
     } catch (e) {
-      console.log(e)
+      console.log(e);
     }
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 main {
-  text-align: center
+  text-align: center;
 }
 
 a {
@@ -92,17 +100,17 @@ a {
 }
 
 .el-card {
-  height: 300px;
+  height: 200px;
 }
 
-.card-body{
+.card-body {
   padding: 0px;
   position: absolute;
   left: 0;
-  background: rgb(15,15,15,0.8);
-  top: 210px;
+  background: rgb(15, 15, 15, 0.8);
+  top: 110px;
   width: 100%;
-  height : 100%;
+  height: 100%;
   -webkit-transition: all 0.4s ease-out;
   -moz-transition: all 0.4s ease-out;
   -ms-transition: all 0.4s ease-out;
@@ -110,11 +118,11 @@ a {
   transition: all 0.4s ease-out;
 }
 
-.el-card:hover .card-body{
+.el-card:hover .card-body {
   top: 0px;
 }
 
-.card-info{
+.card-info {
   padding: 14px;
   height: 100%;
 }
@@ -140,12 +148,12 @@ a {
 .search {
   width: 320px;
   max-width: 90vw;
-  margin: 10px
+  margin: 10px;
 }
 
 .politicians-row {
   width: 90vw;
-  margin: 0 auto
+  margin: 0 auto;
 }
 
 @media only screen and (max-width: 600px) {
@@ -154,7 +162,7 @@ a {
   }
   .el-card {
     height: 280px;
-    min-width: unset
+    min-width: unset;
   }
 }
 </style>
