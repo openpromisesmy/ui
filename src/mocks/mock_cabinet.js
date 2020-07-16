bb44f328302398896ef4f2a60fb53997e7b3154d
Cornelius <35144948+TheCornelius@users.noreply.github.com>
2020-07-16 22:45:01 +0800

    Modified mock_cabinet.js to include up to date cabinet data

--- src/mocks/mock_cabinet.js
+++ src/mocks/mock_cabinet.js
@@ -1,28 +1,41 @@
-const mock_cabinet = {
-    ministries: [
-      {
-        id: 'sadfsadfawefdsfasw4erfsdsdf',
-        name: 'Top Leadership',
-        primary_politician_id: '-L6lZpZcWuJ8siJ3B-yt', // Mahathir
-        primary_title: 'Prime Minister',
-        secondary_politician_ids: [
-          '-LAEFHbjgD01SgG-wwqW',  // Chong Chien Jeng
-          '-L6lYpUWjUHYBOb_fkCB'  // Lim Guan Eng
-        ],
-        secondary_title: 'Senior Minister'
-      },
-      {
-        id: 'dsffsdfsdfsdfsdfsddsfsdfsdfsdf',
-        name: 'Ministry of Education',
-        primary_politician_id: '7TcN9D2JhoYUH1bfS8er',  // Muhyiddin Yassin
-        primary_title: 'Minister of Education',
-        secondary_politician_ids: [
-          '-LAEDr85AyrHvQOBeLJu',  // Abang Jo
-          '31ddGKthhfXp8onAIeQ2'  // Ahmad Faizal Azumu
-        ],
-        secondary_title: 'Deputy Minister'
-      }
-  ]
+const cabinet = {
+    "ministries": [{
+        ministry_id: "mock_top_leadership",
+        name: "Top Leadership",
+        primary_politician_id: "sfpBmOhHuPFYVtq4wQbV",
+        primary_title: "Minister of Top Leadership",
+        secondary_title: "Senior Minister",
+        secondary_politician_ids: ["ATqme0rk0KCw1WNWkksX", "dx8EJBNxptQjOxBqsXDf", "DwdDiRSLYEtdqgp7dPOQ"]
+    }, {
+        ministry_id: "mock_finance",
+        name: "Ministry of Finance",
+        primary_politician_id: "B2O98SBI6pytDmZSpXr2",
+        primary_title: "Minister of Finance",
+        secondary_title: "Deputy Minister of Finance",
+        secondary_politician_ids: ["U7Em33Esl7WgWOHsqJds", "xY1T9zSKBAYEGYNutf9a"]
+    }, {
+        ministry_id: "mock_health",
+        name: "Ministry of Health",
+        primary_politician_id: "5LOz9AGdZs0VQeBqnEgN",
+        primary_title: "Minister of Health",
+        secondary_title: "Deputy Minister of Health",
+        secondary_politician_ids: ["CTucEvtmE96sahlc9EIP", "A3VobkDSr49ILv8Jxdts"]
+    }, {
+        ministry_id: "mock_education",
+        name: "Ministry of Education",
+        primary_politician_id: "DwdDiRSLYEtdqgp7dPOQ",
+        primary_title: "Minister of Education",
+        secondary_title: "Deputy Minister of Education",
+        secondary_politician_ids: ["vV2LzNNnJy8QXXct8Bp3", "MMNBYMZzDOLYIJclnvMI"]
+    }, {
+        ministry_id: "mock_transport",
+        name: "Ministry of Transport",
+        primary_politician_id: "kIIjWY30xdBfXiaFLxPG",
+        primary_title: "Minister of Transport",
+        secondary_title: "Deputy Minister of Transport",
+        secondary_politician_ids: ["GQuj9ZKNrtTPZHNRfRR4"]
+    }]
 }
 
-export default mock_cabinet;
+
+export default cabinet;
