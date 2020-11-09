447f8f921101cc7fe7bbeb002f58802751082c74
Nazreen <nxmohamad@gmail.com>
2020-11-09 23:22:48 +0000

    refactor(views/Politicians): extract Cabinet #290

--- app/src/views/Politicians.vue
+++ app/src/views/Politicians.vue
@@ -4,44 +4,19 @@
       <p>Loading politicians...This will take 1-2 seconds.</p>
       <LoadingSpinner />
     </template>
-        <!-- CABINET VIEW CODE -->
-    <!-- TODO: move into its own component file -->
-    <div v-if="showCabinet && politicians.length > 0 && filteredPoliticians.length !== 0" class="cabinet-view">
-      <h1 class="cabinet-view-title">Cabinet View </h1>
-      <el-row class="cabinet-row" v-for="ministry in cabinet.ministries" :key="ministry.id">
-        <h3 class="ministry-row"> {{ ministry.name }} </h3> <!-- {{ministry.id}} -->
-        <el-col
-          :xs="24"
-          :sm="12"
-          :lg="8"
-          :span="8"
-          class="primary-politician">
-          <h5 class="primary-politician-title">{{ ministry.primary_title }}</h5>
-          <router-link :to="/politician/ + ministry.primary_politician_id">
-            <politician-card :o="politicians.find(politician => politician.id === ministry.primary_politician_id)" />
-          </router-link>
-          <!-- SHOWS PRIMARY POLITICIAN ID <div class="primary-politician-id">{{ ministry.primary_politician_id }}</div> -->
-          <!-- <div class="primary-politician-name"> {{ politicians.find(politician => politician.id === ministry.primary_politician_id).name}}</div> -->
-        </el-col>
-        <el-col
-          :xs="24"
-          :sm="12"
-          :lg="8"
-          :span="8"
-          v-for="s in ministry.secondary_politician_ids"
-          :key="s">
-        <!--SHOWS SECONDARY POLITICIAN ID--> <!-- <div class="secondary-politician-name"> {{ s }} </div> -->
-        <!-- <span>{{ politicians.find(politician => politician.id === s) }}</span> -->
-        <h5 class="secondary-politician-title"> {{ ministry.secondary_title}} </h5>
-        <router-link :to="/politician/ + s">
-          <politician-card :o="politicians.find(politician => politician.id === s)" />
-        </router-link>
-        <!-- <div class="secondary-politician-name"> {{ politicians.find(politician => politician.id === s).name}} </div> -->
-        </el-col>
-      </el-row>
-    </div>
+    <cabinet
+      v-if="
+        showCabinet &&
+        politicians.length > 0 &&
+        filteredPoliticians.length !== 0
+      "
+      :politicians="politicians"
+    />
     <h2>All Politicians</h2>
-    <p>All politicians in the OpenPromises database. If you see any missing, please get in touch with us.</p>
+    <p>
+      All politicians in the OpenPromises database. If you see any missing,
+      please get in touch with us.
+    </p>
     <el-input
       clearable
       placeholder="Search for a politician"
@@ -69,64 +44,53 @@
 </template>
 
 <script>
-import { getPoliticians } from "@/api";
-import { loadCache, updateCache } from "@/utils";
-import LoadingSpinner from "@/components/LoadingSpinner";
-import PoliticianCard from "@/components/PoliticianCard";
-// CABINET VIEW CODE
-import mock_cabinet from '@/mocks/mock_cabinet.js';
-
+import { getPoliticians } from '@/api'
+import { loadCache, updateCache } from '@/utils'
+import LoadingSpinner from '@/components/LoadingSpinner'
+import PoliticianCard from '@/components/PoliticianCard'
+import Cabinet from '@/components/Cabinet'
 
 export default {
-  name: "Politicians",
+  name: 'Politicians',
   data() {
     return {
       politicians: [],
-      search: "",
-      cabinet: {},
-    };
+      search: '',
+    }
   },
-  components: { LoadingSpinner, PoliticianCard },
+  components: { Cabinet, LoadingSpinner, PoliticianCard },
   computed: {
     filteredPoliticians() {
-      return this.politicians.filter(politician => {
-        return politician.name
-          .toLowerCase()
-          .includes(this.search.toLowerCase());
-      });
+      return this.politicians.filter((politician) => {
+        return politician.name.toLowerCase().includes(this.search.toLowerCase())
+      })
     },
     showCabinet() {
-      return false;
+      return process.env.NODE_ENV === 'development'
     },
-  },
-  methods: {
-    async getCabinet() {
-      return mock_cabinet;
-    }
   },
   async created() {
     try {
       // perhaps this caching logic should be in the store instead
       // here we ask the store for the info
       // it is the store's responsibility to check the cache
-      this.politicians = await loadCache(this, "politicians", getPoliticians());
-      this.cabinet = await this.getCabinet();
+      this.politicians = await loadCache(this, 'politicians', getPoliticians())
     } catch (e) {
-      console.log(e);
+      console.log(e)
     }
   },
   async mounted() {
     try {
       this.politicians = await updateCache(
         this,
-        "politicians",
+        'politicians',
         getPoliticians()
-      );
+      )
     } catch (e) {
-      console.log(e);
+      console.log(e)
     }
-  }
-};
+  },
+}
 </script>
 
 <style scoped>
@@ -157,23 +121,5 @@ a {
 
 .politicians-row {
   margin: 0 auto;
-}
-
-.cabinet-view {
-  margin-top: 40px;
-  padding-bottom: 40px;
-}
-
-.cabinet-view-title {
-  margin-bottom: 15px;
-}
-
-.cabinet-row {
-  margin: 0 auto;
-}
-
-.ministry-row {
-  margin-top: 30px;
-  margin-bottom: 0px;
 }
 </style>
