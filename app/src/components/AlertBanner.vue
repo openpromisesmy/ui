69d7e9269d028133494f5de668f14410769989db
Nazreen Mohamad <nxmohamad@gmail.com>
2021-01-15 12:10:05 +0000

    create alert banner

---
+++ app/src/components/AlertBanner.vue
@@ -0,0 +1,20 @@
+<template>
+  <el-alert type="warning" :title="text" />
+</template>
+
+<script>
+export default {
+  name: 'AlertBanner',
+  props: {
+    text: {
+      type: String,
+      required: true,
+      default: ''
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
