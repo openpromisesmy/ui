575a83bdf39f7e984718f4bc2e8a8eb1551ea725
Nazreen <nxmohamad@gmail.com>
2021-01-15 12:39:57 +0000

    build: update env vars (#303)
    * append VUE_APP to env vars
    
    * v

--- app/src/config/index.js
+++ app/src/config/index.js
@@ -1,13 +1,14 @@
 import firebase from 'firebase'
 const config = {
-  apiKey: process.env.GOOGLE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.FIREBASE_DATABASE_URL,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
+  apiKey: process.env.VUE_APP_GOOGLE_API_KEY,
+  authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.VUE_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.VUE_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGING_SENDER_ID
 }
-const facebookAppId = process.env.FACEBOOK_APP_ID
+
+const facebookAppId = process.env.VUE_APP_FACEBOOK_APP_ID
 firebase.initializeApp(config)
 
 const API_URL = process.env.VUE_APP_API_URL
